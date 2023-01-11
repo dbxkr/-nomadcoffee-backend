168ed0d0ddd6e5a7de97e9bc6ee764ad8382b777
Jeon <btt0408@gmail.com>
2023-01-12 00:29:11 +0900

    1.1

--- schema.js
+++ schema.js
@@ -1,9 +1,6 @@
-import {
-  loadFilesSync,
-  makeExecutableSchema,
-  mergeResolvers,
-  mergeTypeDefs,
-} from "graphql-tools";
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(
